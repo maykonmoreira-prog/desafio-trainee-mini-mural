baecc9bd9c4530bb786bd6f93ad5b33073423a67
Maykon334 <maykonboruto334@gmail.com>
2026-06-18 09:14:45 -0300

    Adição dos métodos PUT, GET, READ e DELETE

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,38 @@ app.post("/cadastro", function(req,res){
     })
 });
 
+// Realizando a leitura de todos os registros do banco de dados
+app.get("/", function(req,res){
+    Usuarios.findAll().then(function(usuarios){
+        res.send(usuarios) // Caso for um sucesso, ele enviará as informações em formato .json
+    }).catch(function(erro){
+        res.send("Problema ao buscar os dados. Erro:" + erro); // Para o caso de erro
+    })
+});
+
+// Atualizar o(s) dado(s) de um registro já existente
+app.patch("/atualizar/:id", function(req,res){
+    Usuarios.update({
+        nome: req.body.nome,
+        email: req.body.email
+    },
+        {where: {"id": req.params.id}} // Obriga ao usuario informar o id
+    ).then(function(){
+        res.send("Sucesso ao atualizar os dados do usuário.")
+    }).catch(function(erro){
+        res.send("Problema ao atualizar os dados. Erro: " + erro);
+    })
+})
+
+// Deletar um usuário do banco de dados
+app.delete("/deletar/:id", function(req,res){
+    Usuarios.destroy({where: {"id": req.params.id}}).then(function(){
+        res.send("Usuário deletado com sucesso.");
+    }).catch(function(erro){
+        res.send("Problema ao deletar usuário. Erro:" + erro);
+    })
+})
+
 app.listen(8081, function(){
     console.log("Servidor rodando...");
 });
